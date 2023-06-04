43a86e7bf96e5adba10be2af1a8eca00f44875b3
Drew <95687419+drew7wonders@users.noreply.github.com>
2023-06-04 12:04:20 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -126,7 +126,8 @@ Down.setCollider("rectangle", 13, 35,230, 290);
     if (keyDown(DOWN_ARROW) || mousePressedOver(Down)) {
             sofia.velocityX = 0
             sofia.velocityY = 1
-        text("Working for now", 100, 100);
+          textSize(100)
+        text("Working for now", 300, 200);
         }
 
     // if (mouseOver(Down)) {
